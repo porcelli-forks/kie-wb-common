e8c7c80aa20d152b596d3457b85c13b67c78405f
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2016-02-18 23:08:59 -0500

    BPMSPL-220: new kie-server UI - first version

---
+++ kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-client/src/main/java/org/kie/workbench/common/screens/server/management/client/navigation/template/ServerTemplatePresenter.java
@@ -0,0 +1,233 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.kie.workbench.common.screens.server.management.client.navigation.template;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Event;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.jboss.errai.common.client.api.Caller;
+import org.jboss.errai.common.client.api.ErrorCallback;
+import org.jboss.errai.common.client.api.RemoteCallback;
+import org.kie.server.controller.api.model.events.ServerInstanceDeleted;
+import org.kie.server.controller.api.model.runtime.ServerInstanceKey;
+import org.kie.server.controller.api.model.spec.Capability;
+import org.kie.server.controller.api.model.spec.ContainerSpec;
+import org.kie.server.controller.api.model.spec.ServerTemplate;
+import org.kie.workbench.common.screens.server.management.client.events.AddNewContainer;
+import org.kie.workbench.common.screens.server.management.client.events.ContainerSpecSelected;
+import org.kie.workbench.common.screens.server.management.client.events.ServerInstanceSelected;
+import org.kie.workbench.common.screens.server.management.client.events.ServerTemplateListRefresh;
+import org.kie.workbench.common.screens.server.management.client.navigation.template.copy.CopyPopupPresenter;
+import org.kie.workbench.common.screens.server.management.service.SpecManagementService;
+import org.uberfire.client.mvp.UberView;
+import org.uberfire.mvp.Command;
+import org.uberfire.mvp.ParameterizedCommand;
+import org.uberfire.workbench.events.NotificationEvent;
+
+import static org.uberfire.commons.validation.PortablePreconditions.checkNotNull;
+
+@ApplicationScoped
+public class ServerTemplatePresenter {
+
+    public interface View extends UberView<ServerTemplatePresenter> {
+
+        void clear();
+
+        void setTemplate( final String id,
+                          final String name );
+
+        void selectContainer( final String serverTemplateId,
+                              final String id );
+
+        void selectServerInstance( final String serverTemplateId,
+                                   final String id );
+
+        void addContainer( final String serverTemplateId,
+                           final String containerSpecId,
+                           final String containerName,
+                           final Command onSelect );
+
+        void addServerInstance( final String serverTemplateId,
+                                final String serverInstanceId,
+                                final String serverName,
+                                final Command onSelect );
+
+        void setRulesCapability( boolean value );
+
+        void setProcessCapability( boolean value );
+
+        void setPlanningCapability( final boolean value );
+
+        void confirmRemove( Command command );
+    }
+
+    private final View view;
+    private final CopyPopupPresenter copyPresenter;
+    private final Caller<SpecManagementService> specManagementService;
+    private final Event<NotificationEvent> notification;
+
+    private final Event<AddNewContainer> addNewContainerEvent;
+    private final Event<ContainerSpecSelected> containerSpecSelectedEvent;
+    private final Event<ServerInstanceSelected> serverInstanceSelectedEvent;
+    private final Event<ServerTemplateListRefresh> serverTemplateListRefreshEvent;
+
+    private ServerTemplate serverTemplate;
+
+    @Inject
+    public ServerTemplatePresenter( final View view,
+                                    final CopyPopupPresenter copyPresenter,
+                                    final Caller<SpecManagementService> specManagementService,
+                                    final Event<NotificationEvent> notification,
+                                    final Event<AddNewContainer> addNewContainerEvent,
+                                    final Event<ContainerSpecSelected> containerSpecSelectedEvent,
+                                    final Event<ServerInstanceSelected> serverInstanceSelectedEvent,
+                                    final Event<ServerTemplateListRefresh> serverTemplateListRefreshEvent ) {
+        this.view = view;
+        this.copyPresenter = copyPresenter;
+        this.specManagementService = specManagementService;
+        this.notification = notification;
+        this.addNewContainerEvent = addNewContainerEvent;
+        this.containerSpecSelectedEvent = containerSpecSelectedEvent;
+        this.serverInstanceSelectedEvent = serverInstanceSelectedEvent;
+        this.serverTemplateListRefreshEvent = serverTemplateListRefreshEvent;
+    }
+
+    @PostConstruct
+    public void init() {
+        view.init( this );
+    }
+
+    public View getView() {
+        return view;
+    }
+
+    public ServerTemplate getCurrentServerTemplate() {
+        return serverTemplate;
+    }
+
+    public void setup( final ServerTemplate serverTemplate,
+                       final ContainerSpec firstContainerSpec ) {
+        view.clear();
+        this.serverTemplate = serverTemplate;
+        view.setTemplate( serverTemplate.getId(), serverTemplate.getName() );
+
+        view.setProcessCapability( serverTemplate.getCapabilities().contains( Capability.PROCESS.toString() ) );
+
+        view.setRulesCapability( serverTemplate.getCapabilities().contains( Capability.RULE.toString() ) );
+
+        view.setPlanningCapability( serverTemplate.getCapabilities().contains( Capability.PLANNING.toString() ) );
+
+        if ( firstContainerSpec != null ) {
+            addContainer( firstContainerSpec );
+            for ( final ContainerSpec containerSpec : serverTemplate.getContainersSpec() ) {
+                if ( !containerSpec.getId().equals( firstContainerSpec.getId() ) ) {
+                    addContainer( containerSpec );
+                }
+            }
+            containerSpecSelectedEvent.fire( new ContainerSpecSelected( firstContainerSpec ) );
+        }
+
+        for ( final ServerInstanceKey serverInstanceKey : serverTemplate.getServerInstanceKeys() ) {
+            addServerInstance( serverInstanceKey );
+        }
+    }
+
+    private void addContainer( final ContainerSpec containerSpec ) {
+        view.addContainer( containerSpec.getServerTemplateKey().getId(),
+                           containerSpec.getId(),
+                           containerSpec.getContainerName(),
+                           new Command() {
+                               @Override
+                               public void execute() {
+                                   containerSpecSelectedEvent.fire( new ContainerSpecSelected( containerSpec ) );
+                               }
+                           } );
+    }
+
+    private void addServerInstance( final ServerInstanceKey serverInstanceKey ) {
+        view.addServerInstance( serverInstanceKey.getServerTemplateId(),
+                                serverInstanceKey.getServerInstanceId(),
+                                serverInstanceKey.getServerName(),
+                                new Command() {
+                                    @Override
+                                    public void execute() {
+                                        serverInstanceSelectedEvent.fire( new ServerInstanceSelected( serverInstanceKey ) );
+                                    }
+                                } );
+    }
+
+    public void onContainerSelect( @Observes final ContainerSpecSelected containerSpecSelected ) {
+        view.selectContainer( containerSpecSelected.getContainerSpecKey().getServerTemplateKey().getId(),
+                              containerSpecSelected.getContainerSpecKey().getId() );
+    }
+
+    public void onServerInstanceSelect( @Observes final ServerInstanceSelected serverInstanceSelected ) {
+        view.selectServerInstance( serverInstanceSelected.getServerInstanceKey().getServerTemplateId(),
+                                   serverInstanceSelected.getServerInstanceKey().getServerInstanceId() );
+    }
+
+    public void addNewContainer() {
+        addNewContainerEvent.fire( new AddNewContainer( serverTemplate ) );
+    }
+
+    public void copyTemplate() {
+        copyPresenter.copy( new ParameterizedCommand<String>() {
+            @Override
+            public void execute( final String value ) {
+                specManagementService.call( new RemoteCallback<Void>() {
+                    @Override
+                    public void callback( final Void aVoid ) {
+                        copyPresenter.hide();
+                        serverTemplateListRefreshEvent.fire( new ServerTemplateListRefresh( value ) );
+                    }
+                }, new ErrorCallback<Object>() {
+                    @Override
+                    public boolean error( final Object o,
+                                          final Throwable throwable ) {
+                        copyPresenter.errorDuringProcessing( "Invalid name." );
+                        return false;
+                    }
+                } ).copyServerTemplate( serverTemplate.getId(), value, value );
+            }
+        } );
+    }
+
+    public void removeTemplate() {
+        view.confirmRemove( new Command() {
+            @Override
+            public void execute() {
+                specManagementService.call( new RemoteCallback<Void>() {
+                    @Override
+                    public void callback( final Void aVoid ) {
+                        serverTemplateListRefreshEvent.fire( new ServerTemplateListRefresh() );
+                    }
+                }, new ErrorCallback<Object>() {
+                    @Override
+                    public boolean error( final Object o,
+                                          final Throwable throwable ) {
+                        notification.fire( new NotificationEvent( "Error trying to remove template." ) );
+                        serverTemplateListRefreshEvent.fire( new ServerTemplateListRefresh() );
+                        return false;
+                    }
+                } ).deleteServerTemplate( serverTemplate.getId() );
+            }
+        } );
+    }
+
+}
---
+++ kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-client/src/main/java/org/kie/workbench/common/screens/server/management/client/wizard/container/NewContainerFormView.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.workbench.common.screens.server.management.client.wizard.container;
+
+import java.util.ArrayList;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.Composite;
+import org.gwtbootstrap3.client.ui.TextBox;
+import org.gwtbootstrap3.client.ui.constants.ValidationState;
+import org.gwtbootstrap3.client.ui.html.Div;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.Templated;
+import org.kie.workbench.common.screens.server.management.client.util.ContentChangeHandler;
+import org.kie.workbench.common.screens.server.management.client.util.StyleHelper;
+import org.kie.workbench.common.screens.server.management.client.widget.artifact.ArtifactListWidgetPresenter;
+
+import static org.uberfire.commons.validation.PortablePreconditions.*;
+
+@Dependent
+@Templated
+public class NewContainerFormView extends Composite
+        implements NewContainerFormPresenter.View {
+
+    @DataField("new-container-name-form")
+    Element containerNameGroup = DOM.createDiv();
+
+    @Inject
+    @DataField("new-container-name")
+    TextBox containerName;
+
+    @DataField("new-container-name-help")
+    Element containerNameHelp = DOM.createSpan();
+
+    @DataField("new-group-id-form")
+    Element groupIdGroup = DOM.createDiv();
+
+    @Inject
+    @DataField("new-group-id")
+    TextBox groupId;
+
+    @DataField("new-artifact-id-form")
+    Element artifactIdGroup = DOM.createDiv();
+
+    @Inject
+    @DataField("new-artifact-id")
+    TextBox artifactId;
+
+    @DataField("new-version-form")
+    Element versionGroup = DOM.createDiv();
+
+    @Inject
+    @DataField("new-version")
+    TextBox version;
+
+    @Inject
+    @DataField("content-area")
+    Div content;
+
+    private final ArrayList<ContentChangeHandler> changeHandlers = new ArrayList<ContentChangeHandler>();
+
+    @Override
+    public void init( final NewContainerFormPresenter presenter ) {
+        containerName.addKeyUpHandler( new KeyUpHandler() {
+            @Override
+            public void onKeyUp( KeyUpEvent event ) {
+                if ( presenter.isContainerNameValid() ) {
+                    noErrorOnContainerName();
+                } else {
+                    errorOnContainerName();
+                }
+                fireChangeHandlers();
+            }
+        } );
+
+        groupId.addKeyUpHandler( new KeyUpHandler() {
+            @Override
+            public void onKeyUp( KeyUpEvent event ) {
+                if ( presenter.isGroupIdValid() ) {
+                    noErrorOnGroupId();
+                } else {
+                    errorOnGroupId();
+                }
+                fireChangeHandlers();
+            }
+        } );
+
+        artifactId.addKeyUpHandler( new KeyUpHandler() {
+            @Override
+            public void onKeyUp( KeyUpEvent event ) {
+                if ( presenter.isArtifactIdValid() ) {
+                    noErrorOnArtifactId();
+                } else {
+                    errorOnArtifactId();
+                }
+                fireChangeHandlers();
+            }
+        } );
+
+        version.addKeyUpHandler( new KeyUpHandler() {
+            @Override
+            public void onKeyUp( KeyUpEvent event ) {
+                if ( presenter.isVersionValid() ) {
+                    noErrorOnVersion();
+                } else {
+                    errorOnVersion();
+                }
+                fireChangeHandlers();
+            }
+        } );
+    }
+
+    private void fireChangeHandlers() {
+        for ( final ContentChangeHandler changeHandler : changeHandlers ) {
+            changeHandler.onContentChange();
+        }
+    }
+
+    @Override
+    public void setArtifactListWidgetView( final ArtifactListWidgetPresenter.View view ) {
+        content.add( view );
+    }
+
+    @Override
+    public void clear() {
+        containerName.setText( "" );
+        groupId.setText( "" );
+        artifactId.setText( "" );
+        version.setText( "" );
+
+        noErrors();
+    }
+
+    @Override
+    public void noErrors() {
+        noErrorOnContainerName();
+        noErrorOnGroupId();
+        noErrorOnArtifactId();
+        noErrorOnVersion();
+    }
+
+    @Override
+    public void noErrorOnContainerName() {
+        containerNameHelp.getStyle().setVisibility( Style.Visibility.HIDDEN );
+        StyleHelper.addUniqueEnumStyleName( containerNameGroup, ValidationState.class, ValidationState.NONE );
+    }
+
+    @Override
+    public void noErrorOnGroupId() {
+        StyleHelper.addUniqueEnumStyleName( groupIdGroup, ValidationState.class, ValidationState.NONE );
+    }
+
+    @Override
+    public void noErrorOnArtifactId() {
+        StyleHelper.addUniqueEnumStyleName( artifactIdGroup, ValidationState.class, ValidationState.NONE );
+    }
+
+    @Override
+    public void noErrorOnVersion() {
+        StyleHelper.addUniqueEnumStyleName( versionGroup, ValidationState.class, ValidationState.NONE );
+    }
+
+    @Override
+    public void addContentChangeHandler( final ContentChangeHandler contentChangeHandler ) {
+        changeHandlers.add( checkNotNull( "contentChangeHandler", contentChangeHandler ) );
+    }
+
+    @Override
+    public void setGroupId( final String value ) {
+        groupId.setText( value );
+    }
+
+    @Override
+    public void setAtifactId( final String value ) {
+        artifactId.setText( value );
+    }
+
+    @Override
+    public void setVersion( final String value ) {
+        version.setText( value );
+    }
+
+    @Override
+    public String getContainerName() {
+        return containerName.getText();
+    }
+
+    @Override
+    public String getGroupId() {
+        return groupId.getText();
+    }
+
+    @Override
+    public String getArtifactId() {
+        return artifactId.getText();
+    }
+
+    @Override
+    public String getVersion() {
+        return version.getText();
+    }
+
+    @Override
+    public void errorOnContainerName() {
+        StyleHelper.addUniqueEnumStyleName( containerNameGroup, ValidationState.class, ValidationState.ERROR );
+    }
+
+    @Override
+    public void errorOnContainerName( final String message ) {
+        errorOnContainerName();
+        containerNameHelp.getStyle().setVisibility( Style.Visibility.VISIBLE );
+        containerNameHelp.setInnerText( message );
+    }
+
+    @Override
+    public void errorOnGroupId() {
+        StyleHelper.addUniqueEnumStyleName( groupIdGroup, ValidationState.class, ValidationState.ERROR );
+    }
+
+    @Override
+    public void errorOnArtifactId() {
+        StyleHelper.addUniqueEnumStyleName( artifactIdGroup, ValidationState.class, ValidationState.ERROR );
+    }
+
+    @Override
+    public void errorOnVersion() {
+        StyleHelper.addUniqueEnumStyleName( versionGroup, ValidationState.class, ValidationState.ERROR );
+    }
+
+}
