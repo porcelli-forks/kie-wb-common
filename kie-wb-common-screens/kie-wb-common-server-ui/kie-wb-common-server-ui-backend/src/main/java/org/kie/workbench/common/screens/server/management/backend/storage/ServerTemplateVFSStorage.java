69c480fc0dcd9006418fd17b397069ccd759dbce
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2016-02-22 01:14:57 -0500

    ops

--- kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-backend/src/main/java/org/kie/workbench/common/screens/server/management/backend/storage/ServerTemplateVFSStorage.java
+++ kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-backend/src/main/java/org/kie/workbench/common/screens/server/management/backend/storage/ServerTemplateVFSStorage.java
@@ -15,13 +15,55 @@
 
 package org.kie.workbench.common.screens.server.management.backend.storage;
 
+import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 
+import org.kie.server.controller.api.model.spec.ServerTemplate;
+import org.kie.server.controller.api.model.spec.ServerTemplateKey;
+import org.kie.server.controller.api.storage.KieServerTemplateStorage;
 import org.kie.server.controller.impl.storage.InMemoryKieServerTemplateStorage;
 
 @ApplicationScoped
-public class ServerTemplateVFSStorage extends InMemoryKieServerTemplateStorage {    // TODO for now use the inmemory storage
+public class ServerTemplateVFSStorage implements KieServerTemplateStorage {    // TODO for now use the inmemory storage
+
+    private final KieServerTemplateStorage storage;
 
     public ServerTemplateVFSStorage() {
+        storage = InMemoryKieServerTemplateStorage.getInstance();
+    }
+
+    @Override
+    public ServerTemplate store( final ServerTemplate serverTemplate ) {
+        return storage.store( serverTemplate );
+    }
+
+    @Override
+    public List<ServerTemplateKey> loadKeys() {
+        return storage.loadKeys();
+    }
+
+    @Override
+    public List<ServerTemplate> load() {
+        return storage.load();
+    }
+
+    @Override
+    public ServerTemplate load( final String identifier ) {
+        return storage.load( identifier );
+    }
+
+    @Override
+    public boolean exists( final String identifier ) {
+        return storage.exists( identifier );
+    }
+
+    @Override
+    public ServerTemplate update( final ServerTemplate serverTemplate ) {
+        return storage.update( serverTemplate );
+    }
+
+    @Override
+    public ServerTemplate delete( final String identifier ) {
+        return storage.delete( identifier );
     }
 }
